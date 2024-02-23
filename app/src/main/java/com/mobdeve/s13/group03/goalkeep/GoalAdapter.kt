52eadba028ad9614e2de716d4bcba7b3bf557387
Alfonso Miguel Cruz <alfonso_cruz@dlsu.edu.ph>
2024-02-23 21:56:34 +0800

    Added TODO for GoalAdapter and ViewGoalActivity

--- app/src/main/java/com/mobdeve/s13/group03/goalkeep/GoalAdapter.kt
+++ app/src/main/java/com/mobdeve/s13/group03/goalkeep/GoalAdapter.kt
@@ -1,5 +1,6 @@
 package com.mobdeve.s13.group03.goalkeep
 
+import android.app.Activity
 import android.content.Intent
 import android.view.LayoutInflater
 import android.view.ViewGroup
@@ -13,7 +14,7 @@ class GoalAdapter(private val goals: ArrayList<Goal>, private val tasksList: Arr
         val tempViewHolder = GoalViewHolder(goalViewBinding, tasksList)
 
         tempViewHolder.itemView.setOnClickListener{
-            var goalInt = Intent(parent.context, ViewGoalActivity::class.java)
+            val goalInt = Intent(parent.context, ViewGoalActivity::class.java)
             // Title of the Goal
             goalInt.putExtra(ViewGoalActivity.TITLE_KEY, goalViewBinding.tvGoalName.text.toString())
             // Time Expected to complete the Goal
@@ -28,6 +29,8 @@ class GoalAdapter(private val goals: ArrayList<Goal>, private val tasksList: Arr
             goalInt.putExtra(ViewGoalActivity.PROGRESS_KEY, computeProgress(goals[tempViewHolder.bindingAdapterPosition]))
             // Get the state of the progress, used to enable the button that completes a goal
             goalInt.putExtra(ViewGoalActivity.STATE_KEY, computeProgress(goals[tempViewHolder.bindingAdapterPosition]) == 100)
+
+            /* TODO: Find a way to Send the Intent to ViewGoalActivity */
         }
 
         return tempViewHolder
