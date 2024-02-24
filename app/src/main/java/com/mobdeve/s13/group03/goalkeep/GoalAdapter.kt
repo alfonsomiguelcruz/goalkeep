9ebe947957aeee48245343f5f7ef1de6923408dd
patmnic <patricia_nicole_martinez@dlsu.edu.ph>
2024-02-24 18:05:08 +0800

    Update GoalAdapter.kt

--- app/src/main/java/com/mobdeve/s13/group03/goalkeep/GoalAdapter.kt
+++ app/src/main/java/com/mobdeve/s13/group03/goalkeep/GoalAdapter.kt
@@ -14,26 +14,6 @@ class GoalAdapter(private val goals: ArrayList<Goal>, private val tasksList: Arr
         val tempViewHolder = GoalViewHolder(goalViewBinding, tasksList)
         this.parent = parent
 
-//        tempViewHolder.itemView.setOnClickListener{
-//            val goalInt = Intent(parent.context, ViewGoalActivity::class.java)
-//            // Title of the Goal
-//            goalInt.putExtra(ViewGoalActivity.TITLE_KEY, goalViewBinding.tvGoalName.text.toString())
-//            // Time Expected to complete the Goal
-//            goalInt.putExtra(ViewGoalActivity.TIME_KEY, goalViewBinding.tvGoalTimeExpected.text.toString())
-//            // Priority of the Goal
-//            goalInt.putExtra(ViewGoalActivity.PRIORITY_KEY, goalViewBinding.tvGoalPriority.toString())
-//            // Tag of the Goal
-//            goalInt.putExtra(ViewGoalActivity.TAG_KEY, goalViewBinding.tvGoalTag.text.toString())
-//            // Description of the Goal
-//            goalInt.putExtra(ViewGoalActivity.DESCRIPTION_KEY, goals[tempViewHolder.bindingAdapterPosition].description)
-//            // Get the progress percentage (min: 0, max: 100)
-//            goalInt.putExtra(ViewGoalActivity.PROGRESS_KEY, computeProgress(goals[tempViewHolder.bindingAdapterPosition]))
-//            // Get the state of the progress, used to enable the button that completes a goal
-//            goalInt.putExtra(ViewGoalActivity.STATE_KEY, computeProgress(goals[tempViewHolder.bindingAdapterPosition]) == 100)
-//
-//            /* TODO: Find a way to Send the Intent to ViewGoalActivity */
-//        }
-
         return tempViewHolder
     }
 
