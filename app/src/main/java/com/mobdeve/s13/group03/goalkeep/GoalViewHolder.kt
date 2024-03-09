0f560ddf21da65318379d6d156f12347b42e7abd
Alfonso Miguel Cruz <alfonso_cruz@dlsu.edu.ph>
2024-03-09 16:38:08 +0800

    Fixed colors a bit

--- app/src/main/java/com/mobdeve/s13/group03/goalkeep/GoalViewHolder.kt
+++ app/src/main/java/com/mobdeve/s13/group03/goalkeep/GoalViewHolder.kt
@@ -3,12 +3,14 @@ package com.mobdeve.s13.group03.goalkeep
 import android.annotation.SuppressLint
 import android.app.Activity
 import android.content.res.ColorStateList
+import android.graphics.drawable.Drawable
 import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.RecyclerView
 import com.mobdeve.s13.group03.goalkeep.databinding.GoalListLayoutBinding
 
 class GoalViewHolder(private val goalVB: GoalListLayoutBinding, private val tasksList: ArrayList<Task>) : RecyclerView.ViewHolder(goalVB.root) {
 
+    @SuppressLint("ResourceAsColor")
     fun bindGoalData(g: Goal) {
         this.goalVB.tvGoalName.text = g.name
         this.goalVB.tvGoalPriority.text = g.priority
@@ -18,8 +20,8 @@ class GoalViewHolder(private val goalVB: GoalListLayoutBinding, private val task
 
         /* Issue #1 The */
         this.goalVB.llGoalBody.background.setTint(getGoalColor(g.priority))
-//        this.goalVB.pbGoals.background.setTint(getGoalSubColor(g.priority))
-//        this.goalVB.tvGoalPriority.setTextColor(getGoalSubColor(g.priority))
+        this.goalVB.pbGoals.background.setTint(getGoalSubColor(g.priority))
+        this.goalVB.tvGoalPriority.setTextColor(getGoalSubColor(g.priority))
     }
 
     private fun computeProgress(g: Goal): Int {
@@ -49,7 +51,7 @@ class GoalViewHolder(private val goalVB: GoalListLayoutBinding, private val task
             "Medium" -> R.color.medium_default
             "Low" -> R.color.low_default
             else -> {
-                R.color.black
+                R.drawable.corners_gt
             }
         }
 
@@ -62,7 +64,7 @@ class GoalViewHolder(private val goalVB: GoalListLayoutBinding, private val task
             "Medium" -> R.color.medium_sub
             "Low" -> R.color.low_sub
             else -> {
-                R.color.black
+                R.color.white
             }
         }
 
