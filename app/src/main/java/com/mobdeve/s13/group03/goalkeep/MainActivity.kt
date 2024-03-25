a0f4379eef52eece84309b7dda02ccb4e101c44f
Alfonso Miguel Cruz <alfonso_cruz@dlsu.edu.ph>
2024-03-25 11:50:04 +0800

    Made UI for Goal sort and filter

--- app/src/main/java/com/mobdeve/s13/group03/goalkeep/MainActivity.kt
+++ app/src/main/java/com/mobdeve/s13/group03/goalkeep/MainActivity.kt
@@ -1,5 +1,9 @@
 package com.mobdeve.s13.group03.goalkeep
 
+import android.annotation.SuppressLint
+import android.app.DatePickerDialog
+import android.app.Dialog
+import android.app.TimePickerDialog
 import android.content.Intent
 import android.os.Bundle
 import android.view.View
@@ -8,6 +12,8 @@ import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.mobdeve.s13.group03.goalkeep.databinding.ActivityMainBinding
+import com.mobdeve.s13.group03.goalkeep.databinding.GoalSortFilterDialogLayoutBinding
+import java.util.Calendar
 
 class MainActivity : AppCompatActivity() {
     private val goals = GoalDataGenerator.generateGoalData()
@@ -17,6 +23,12 @@ class MainActivity : AppCompatActivity() {
     private lateinit var goalsAdapter : GoalAdapter
     private lateinit var goalsTouchHelper: ItemTouchHelper
 
+    private var yearInput : Int = Calendar.getInstance().get(Calendar.YEAR)
+    private var monthInput : Int = Calendar.getInstance().get(Calendar.MONTH) - 1
+    private var dayInput : Int = Calendar.getInstance().get(Calendar.DAY_OF_MONTH)
+    private var hourInput : Int = Calendar.getInstance().get(Calendar.HOUR)
+    private var minuteInput : Int = Calendar.getInstance().get(Calendar.MINUTE)
+
     companion object {
         const val TITLE_KEY = "TITLE_KEY"
         const val DESCRIPTION_KEY = "DESCRIPTION_KEY"
@@ -25,6 +37,7 @@ class MainActivity : AppCompatActivity() {
         const val RESPONSE_KEY = "RESPONSE_KEY"
     }
 
+    @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -90,6 +103,64 @@ class MainActivity : AppCompatActivity() {
                 vb.ibGoalFilter.visibility = View.VISIBLE
                 vb.ibSettings.visibility = View.VISIBLE
             }
+        }
+
+        vb.ibGoalFilter.setOnClickListener {
+            val d = Dialog(this)
+//            val layoutInflater = this.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+            val vb = GoalSortFilterDialogLayoutBinding.inflate(layoutInflater)
+
+            d.setContentView(vb.root)
+
+            vb.clGoalFilterTimeExpectedStartDate.setOnClickListener {
+                val dateStartDialog = DatePickerDialog(this, { view, year, monthOfYear, dayOfMonth ->
+                    vb.tvGoalFilterTimeExpectedStartDate.text = "${DateHelper.getMonthName(monthOfYear + 1)} $dayOfMonth, $year"
+                    yearInput = year
+                    monthInput = monthOfYear
+                    dayInput = dayOfMonth
+                },  yearInput, monthInput, dayInput)
+                dateStartDialog.show()
+            }
+
+            vb.clGoalFilterTimeExpectedStartTime.setOnClickListener {
+                val timeStartDialog = TimePickerDialog(this, { view, hourOfDay, minute ->
+                    vb.tvGoalFilterTimeExpectedStartTime.text = "${DateHelper.getNonMilitaryHour(hourOfDay)}: $minute ${DateHelper.getAMPM(hourOfDay)}"
+                    hourInput = hourOfDay
+                    minuteInput = minute
+                },  hourInput, minuteInput, false)
+                timeStartDialog.show()
+            }
+
+            vb.clGoalFilterTimeExpectedEndDate.setOnClickListener {
+                val dateEndDialog = DatePickerDialog(this, { view, year, monthOfYear, dayOfMonth ->
+                    vb.tvGoalFilterTimeExpectedEndDate.text = "${DateHelper.getMonthName(monthOfYear + 1)} $dayOfMonth, $year"
+                    yearInput = year
+                    monthInput = monthOfYear
+                    dayInput = dayOfMonth
+                },  yearInput, monthInput, dayInput)
+                dateEndDialog.show()
+            }
+
+            vb.clGoalFilterTimeExpectedEndTime.setOnClickListener {
+                val timeEndDialog = TimePickerDialog(this, { view, hourOfDay, minute ->
+                    vb.tvGoalFilterTimeExpectedEndTime.text = "${DateHelper.getNonMilitaryHour(hourOfDay)}: $minute ${DateHelper.getAMPM(hourOfDay)}"
+                    hourInput = hourOfDay
+                    minuteInput = minute
+                },  hourInput, minuteInput, false)
+                timeEndDialog.show()
+            }
+
+            vb.btnGoalSortFilterCancel.setOnClickListener {
+                d.dismiss()
+            }
+
+            // TODO: After closing the dialog, update recycler view contents
+            vb.btnGoalSortFilterApply.setOnClickListener {
+                d.dismiss()
+            }
+
+            d.setCancelable(true)
+            d.show()
         }
     }
 }
